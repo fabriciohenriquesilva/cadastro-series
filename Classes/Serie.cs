af3cddbfc7d125273ad8dbeb78b519ae21b6bb9b
Fabricio Henrique da Silva <43080818+fabriciohenriquesilva@users.noreply.github.com>
2021-03-10 19:42:09 -0300

    cadastro de serie em c#

---
+++ Classes/Serie.cs
@@ -0,0 +1,51 @@
+namespace Series
+{
+    public class Serie : EntidadeBase
+    {
+        // Atributos
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+
+        private bool Excluido { get; set; }
+        // Metodos
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)    
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+            this.Excluido = false;
+        }   
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Gênero: " + this.Genero + System.Environment.NewLine;
+            retorno += "Titulo: " + this.Titulo + System.Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + System.Environment.NewLine;
+            retorno += "Ano de Início: " + this.Ano + System.Environment.NewLine;
+            retorno += "Excluído: " + this.Excluido + System.Environment.NewLine;
+            return retorno;
+        }
+
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+        }
+        public int retornaId()
+        {
+            return this.Id;
+        }
+        public bool retornaExcluido()
+        {
+            return this.Excluido;
+        }
+        public void Excluir()
+        {
+            this.Excluido = true;
+        }
+    }
+}
